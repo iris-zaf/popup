38ce484948de9b1c1f1983afe94dc44ed1575da9
Iris <iriskalogirou1@gmail.com>
2025-06-08 19:22:14 +0300

    production

--- config/config.php
+++ config/config.php
@@ -1,7 +1,8 @@
 <?php
 //for localhost
-$base = (isset($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'];
-//for live: $base = 'https://' . $_SERVER['HTTP_HOST'];
+//$base = (isset($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'];
+//for live:
+$base = 'https://' . $_SERVER['HTTP_HOST'];
 define('PLUGIN_ROOT', $base . '/');
 define('PLUGIN_ASSETS', PLUGIN_ROOT . 'assets/');
 define('PLUGIN_UPLOADS',  PLUGIN_ROOT . 'uploads/images/popup/');
--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,9 @@
     <?php
     require_once __DIR__ . '/../config/config.php';
     ?>
-    <!-- FOR LIVE <script src="/assets/js/frontend-scripts.js"></script> -->
-    <script src="<?= PLUGIN_ASSETS ?>js/frontend-scripts.js"></script>
+    <!-- FOR production -->
+    <script src="/assets/js/frontend-scripts.js"></script>
+    <!-- FOR LIVE <script src="<?= PLUGIN_ASSETS ?>js/frontend-scripts.js"></script> -->
     </body>
 
     </html>
