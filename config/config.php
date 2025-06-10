b50e242935678fe3d499e5a93b6f117700415efc
Iris <iriskalogirou1@gmail.com>
2025-06-11 00:08:56 +0300

    config

--- config/config.php
+++ config/config.php
@@ -1,18 +1,17 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 //for localhost
-$base = (isset($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'];
+//$base = (isset($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'];
 //for live:
-//$base = 'https://' . $_SERVER['HTTP_HOST'];
+$base = 'https://' . $_SERVER['HTTP_HOST'];
 define('PLUGIN_ROOT', $base . '/');
 define('PLUGIN_ASSETS', PLUGIN_ROOT . 'assets/');
 define('PLUGIN_UPLOADS', PLUGIN_ROOT . 'uploads/images/popup/');
 
 
-$mongoUri = 'mongodb+srv://irisk:kY0Eak8EAHLj0Esm@popup.mmt3buw.mongodb.net/?retryWrites=true&w=majority&appName=popup';
 
 //mongodb connection --Render --
-//$mongoUri = getenv('MONGO_URI');
+$mongoUri = getenv('MONGO_URI');
 
 try {
     $mongoClient = new MongoDB\Client($mongoUri);
