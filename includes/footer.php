452aee46833bf2c011b36b83893668df386f5a37
Iris <iriskalogirou1@gmail.com>
2025-06-14 09:48:27 +0300

    echo for debug

--- includes/footer.php
+++ includes/footer.php
@@ -4,11 +4,9 @@
 
     $page = $_GET['page'] ?? 'home';
     $settings = get_popup_settings($page);
-
-    include $_SERVER['DOCUMENT_ROOT'] . '/popup/views/popup-view.php';
+    require_once __DIR__ . '/../views/popup-view.php';
+    echo "" . $_SERVER['DOCUMENT_ROOT'] . "/views/popup-view.php";
     ?>
-   echo "
-   <pre>" . $_SERVER['DOCUMENT_ROOT'] . "/popup/views/popup-view.php</pre>";
    <!--  FOR production -->
    <script src="/assets/js/frontend-scripts.js"></script>
    <!-- FOR localhost
